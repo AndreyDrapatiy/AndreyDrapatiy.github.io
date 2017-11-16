6e0d278a2afa4fff53d860ea27741d04d31f5e8d
unknown <drapatiy92@gmail.com>
2017-11-16 16:35:44 +0200

    js add

--- wintest/js/main.js
+++ wintest/js/main.js
@@ -1,6 +1,8 @@
 function openModal() {
     document.body.style.overflowY = 'hidden';
+    document.body.style.marginRight = '16px';
 }
 function closeModal() {
     document.body.style.overflowY = 'auto';
+    document.body.style.marginRight = '0px';
 }
