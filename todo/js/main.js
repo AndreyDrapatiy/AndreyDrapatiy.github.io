8ad51f659949f25eb6050e90025c69ae2c129c43
unknown <drapatiy92@gmail.com>
2017-11-17 16:05:23 +0200

    todolist fix

--- todo/js/main.js
+++ todo/js/main.js
@@ -1,17 +1,3 @@
-// dinamic hamburger //
-var forEach = function (t, o, r) {
-    if ("[object Object]" === Object.prototype.toString.call(t)) for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t); else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
-};
-
-var hamburgers = document.querySelectorAll(".hamburger");
-if (hamburgers.length > 0) {
-    forEach(hamburgers, function (hamburger) {
-        hamburger.addEventListener("click", function () {
-            this.classList.toggle("is-active");
-        }, false);
-    });
-}
-// dinamic hamburger //
 
 
 // sidebar opensclose//
@@ -19,19 +5,33 @@ if (hamburgers.length > 0) {
 document.getElementById('sidebar-open-close').addEventListener('click', function (el) {
 
     el.preventDefault();
-
+    var hamburger = document.getElementById('sidebar-open-close');
     var sideClass = document.getElementById('sidebar').classList;
     var innerColor = document.getElementById('hamburger-inner');
     if (sideClass.contains('sidebar-close')) {
         sideClass.remove('sidebar-close');
         sideClass.add('sidebar-open');
         innerColor.style.backgroundColor = "#f50057";
-
+        hamburger.classList.toggle('is-active');
     }
     else if (sideClass.contains('sidebar-open')) {
         sideClass.remove('sidebar-open');
         sideClass.add('sidebar-close');
         innerColor.style.backgroundColor = "#fff";
+        hamburger.classList.toggle('is-active');
+
+
+    }
+});
+document.getElementById('sidebar').addEventListener('click', function () {
+    var hamburger = document.getElementById('sidebar-open-close');
+    var sideClass = document.getElementById('sidebar').classList;
+    var innerColor = document.getElementById('hamburger-inner');
+    if (sideClass.contains('sidebar-open')) {
+        sideClass.remove('sidebar-open');
+        sideClass.add('sidebar-close');
+        innerColor.style.backgroundColor = "#fff";
+        hamburger.classList.toggle('is-active');
 
     }
 });
@@ -84,57 +84,67 @@ function closeCreateNew() {
     document.getElementById('createNew').style.display = 'inline-block';
 }
 
-document.getElementById('add').addEventListener('click', function () {
-
-    var titleNew = document.getElementById('titleNew').value;
-    var contentNew = document.getElementById('contentNew').value;
-
-    function formatDate(date) {
-
-        var dd = date.getDate();
-        if (dd < 10) dd = '0' + dd;
-
-        var mm = date.getMonth() + 1;
-        if (mm < 10) mm = '0' + mm;
-
-        var yy = date.getFullYear() % 100;
-        if (yy < 10) yy = '0' + yy;
-
-        return dd + '.' + mm + '.' + yy;
+document.getElementById('add').addEventListener('click', addValues());
+document.addEventListener('keydown', function () {
+    var check = document.getElementById('new-item-input').style.display;
+    if(check !== 'none' && event.keyCode === 13){
+        addValues()
     }
-
-    var thisItemDate = new Date(2014, 0, 30);
-
-    var template = '<div class="todo-item todo-item-active">' +
-        '<div class="todo-item-title">' + titleNew + '</div>' +
-        '<div class="todo-item-text">' + contentNew + '</div>' +
-        '<div class="actions-time">' +
-        '<div class="time">' + formatDate(thisItemDate) + '</div>' +
-        '<ul  class="done-remove-btn">' +
-        '<li  id = "complet-btn" onclick="moveToCompleted(this)"><i class="material-icons">&#xE876;</i></li>' +
-        '<li  id = "delete-btn" onclick="removeItem(this)"><i class="material-icons">&#xE15B;</i></li>' +
-        '</ul>' +
-        '</div>';
-    if (contentNew !== '') {
-        document.getElementById('items-all').innerHTML += template;
-        document.getElementById('titleNew').value = '';
-        document.getElementById('contentNew').value = '';
-        document.getElementById('new-item-input').style.display = 'none';
-        document.getElementById('items-all').style.display = 'block';
-        document.getElementById('createNew').style.display = 'inline-block';
-    }
-    else {
-        alert("Please enter an TODO content to continue.")
-    }
-
-
 });
+
+    function addValues() {
+        var titleNew = document.getElementById('titleNew').value;
+        var contentNew = document.getElementById('contentNew').value;
+
+        function formatDate(date) {
+
+            var dd = date.getDate();
+            if (dd < 10) dd = '0' + dd;
+
+            var mm = date.getMonth() + 1;
+            if (mm < 10) mm = '0' + mm;
+
+            var yy = date.getFullYear() % 100;
+            if (yy < 10) yy = '0' + yy;
+
+            return dd + '.' + mm + '.' + yy;
+        }
+
+        var thisItemDate = new Date(2014, 0, 30);
+
+        var template = '<div class="todo-item todo-item-active">' +
+            '<div class="todo-item-title">' + titleNew + '</div>' +
+            '<div class="todo-item-text">' + contentNew + '</div>' +
+            '<div class="actions-time">' +
+            '<div class="time">' + formatDate(thisItemDate) + '</div>' +
+            '<ul  class="done-remove-btn">' +
+            '<li  id = "complet-btn" onclick="moveToCompleted(this)"><i class="material-icons">&#xE876;</i></li>' +
+            '<li  id = "delete-btn" onclick="removeItem(this)"><i class="material-icons">&#xE15B;</i></li>' +
+            '</ul>' +
+            '</div>';
+        if (contentNew !== '') {
+            document.getElementById('items-all').innerHTML += template;
+            document.getElementById('titleNew').value = '';
+            document.getElementById('contentNew').value = '';
+            document.getElementById('new-item-input').style.display = 'none';
+            document.getElementById('items-all').style.display = 'block';
+            document.getElementById('createNew').style.display = 'inline-block';
+        }
+        else {
+            alert("Please enter an TODO content to continue.")
+        }
+
+    }
+
+
+
+
 // create new //
 
 
 //remove item//
 function removeItem(param) {
-    param.parentNode.parentNode.parentNode.remove() // lol конечно
+    param.parentNode.parentNode.parentNode.remove()
 }
 
 //remove item//
