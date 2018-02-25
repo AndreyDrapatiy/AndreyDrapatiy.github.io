19ac1885900f19820155351a952e41f452a4ac9a
Your Name <drapatiy92@gmail.com>
2018-02-25 17:32:09 +0300

    indicators

--- slider/dist/bundle.js
+++ slider/dist/bundle.js
@@ -1,1 +1,182 @@
-!function(e){function t(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}var n={};t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:r})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},t.p="",t(t.s=0)}([function(e,t,n){"use strict";function r(){function e(){var e=getComputedStyle(o);return r=parseInt(e.marginLeft)}function t(){var e=u-event.clientX;(o.style.marginLeft=-e+r)<0&&(o.style.marginLeft=-e+r)>-a&&(o.style.marginLeft=-e+r+"px")}function n(){o.classList.add("transition");var n=event.clientX;if(document.removeEventListener("mousemove",t),n<u)if(r!==-a){var s=i-r;o.style.marginLeft=-s+"px"}else o.style.marginLeft="0px";else if(n>u)if(0!==r){var l=i+r;o.style.marginLeft=l+"px"}else o.style.marginLeft=-a+"px";e()}event.preventDefault();var r,s=o.getElementsByTagName("img").length,a=i*s-i,u=event.clientX;document.addEventListener("mousemove",t),document.addEventListener("mouseup",n),o.classList.remove("transition"),e()}Object.defineProperty(t,"__esModule",{value:!0});var o,s=n(1),i=(n.n(s),document.getElementsByClassName("carousel")[0].offsetWidth);document.addEventListener("mousedown",r);for(var a=document.getElementsByClassName("carousel"),u=0,l=a.length;u<l;u++)a[u].getElementsByClassName("content")[0].addEventListener("mousedown",function(){return o=this});for(var m=document.getElementsByTagName("img"),u=0,l=m.length;u<l;u++)m[u].setAttribute("width",i+"px")},function(e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+let currentWidth = document.getElementsByClassName('carousel')[0].offsetWidth;// текущий размер одного слайдера в браузере
+let slider; //тут хранится текущий слайдер над которым водят указателем
+
+document.addEventListener('mousedown', dragStart);
+
+
+for (let currentSlider = document.getElementsByClassName('carousel'), j = 0, lj = currentSlider.length; j < lj; j++) {
+
+    currentSlider[j].getElementsByClassName('content')[0].addEventListener('mousedown', function () {
+        return slider = this;
+    });
+
+    for (let i = 0; i < currentSlider[j].getElementsByTagName('img').length; i++) {
+        //добывим индикаторы к слайдерам в от количества картинок в них
+        currentSlider[j].getElementsByTagName('ul')[0].innerHTML += '<li class="indicator" id="' + i + '"></li>';
+        currentSlider[j].getElementsByTagName('li')[0].classList.add('active');
+    }
+
+}
+
+for (let image = document.getElementsByTagName('img'), j = 0, lj = image.length; j < lj; j++) {
+    image[j].setAttribute('width', currentWidth + 'px');
+}
+
+
+function setIndicator() {
+
+    //индикатор становится активным так:
+    //текущий отступ контейнера делим на ширину одного слайда
+    //получаем число которое будет соответсвовать номеру индикатора
+    //например  отступ контейнера 0/щирину то номер индикатора 0
+    //если отступ -675px/ширина(675) = 1(значить слайдер прокрутитили на один слайд)
+    //индикатору с индексом 1 присвоен класс active
+
+    let index = Math.abs(parseInt(getComputedStyle(slider).marginLeft));
+
+    for (let unsetInd = slider.parentNode.getElementsByTagName('li'), j = 0, lj = unsetInd.length; j < lj; j++) {
+        unsetInd[j].classList.remove('active');
+    }
+    slider.parentNode.getElementsByTagName('li')[index / currentWidth].classList.add('active');
+}
+
+
+function dragStart() {
+
+    let startX = event.clientX;
+    let params = {
+        currentMargin: null,
+        maxMargin: currentWidth * slider.getElementsByTagName('img').length - currentWidth,
+        imgLength: slider.getElementsByTagName('img').length,
+    };
+
+    function currentMarginFn() {
+        let x = getComputedStyle(slider);
+        return params.currentMargin = parseInt(x.marginLeft);
+    }
+
+    event.preventDefault();
+
+    document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', moveEnd);
+
+
+    function mouseMove() {
+        let move = startX - event.clientX;
+        if (((slider.style.marginLeft = -move + params.currentMargin) < 0) && ((slider.style.marginLeft = -move + params.currentMargin) > -params.maxMargin)) {
+            slider.style.marginLeft = -move + params.currentMargin + 'px';
+        }
+    }
+
+    function moveEnd() {
+        slider.classList.add('transition');// добавим transition после того как сладйр отпустили, раньше нельзя, будет не корректное перетаскивание мышкой
+
+        let endX = event.clientX;
+
+        document.removeEventListener('mousemove', mouseMove);
+
+        if (endX < startX) {
+                                //выясним напрваление разницей точки в которой был mousedown и mouseup
+                                //если точка старта по X например больше точки конца, значить танут влево
+                                //выясняем сколько уже протянуто, и сколько осталось
+                                //остаток присваиваем в свойство marginLeft
+            if (params.currentMargin !== -params.maxMargin) {
+                let left = currentWidth - params.currentMargin;
+                slider.style.marginLeft = -left + 'px';
+            }
+            else {
+                slider.style.marginLeft = 0 + 'px';
+            }
+            }
+        else if (endX > startX) {
+            if (params.currentMargin !== 0) {
+                let right = currentWidth + params.currentMargin;
+                slider.style.marginLeft = right + 'px';
+            }
+            else {
+                slider.style.marginLeft = -params.maxMargin + 'px';
+            }
+        }
+        currentMarginFn(); //обновляем значение margin
+        setTimeout(setIndicator, 190);// вызов присвоения нового индикатора должен быть слегка позже, что бы transition успел закончить
+    }
+
+    slider.classList.remove('transition'); //нужно снова удалить для красивого перетасивание указателем
+    currentMarginFn();//обновляем значение margin
+}
+
+
+
+
+
+/***/ })
+/******/ ]);
--- slider/src/script.js
+++ slider/src/script.js
@@ -1,57 +1,102 @@
-import './style.scss'
+let currentWidth = document.getElementsByClassName('carousel')[0].offsetWidth;// текущий размер одного слайдера в браузере
+let slider; //тут хранится текущий слайдер над которым водят указателем
 
-var currentWidth = document.getElementsByClassName('carousel')[0].offsetWidth;
-var slider;
+document.addEventListener('mousedown', function (event) {
+    let target = event.target; // где был клик?
 
-document.addEventListener('mousedown', dragStart);
+    if (target.tagName === 'IMG') {
+        dragStart()
+    }
+    else if (target.tagName === 'LI') {
+        moveToSlide(target);
+    }
+});
 
-for (var currentSlider = document.getElementsByClassName('carousel'), j = 0, lj = currentSlider.length; j < lj; j++) {
+for (let currentSlider = document.getElementsByClassName('carousel'), j = 0, lj = currentSlider.length; j < lj; j++) {
+
+    for (let i = 0; i < currentSlider[j].getElementsByTagName('img').length; i++) {
+        //добывим индикаторы к слайдерам в от количества картинок в них
+        currentSlider[j].getElementsByTagName('ul')[0].innerHTML += '<li class="indicator" id="' + i + '"><a href="#"></a></li>';
+        currentSlider[j].getElementsByTagName('li')[0].classList.add('active');
+    }
     currentSlider[j].getElementsByClassName('content')[0].addEventListener('mousedown', function () {
         return slider = this;
     });
 }
 
-for (var image = document.getElementsByTagName('img'), j = 0, lj = image.length; j < lj; j++) {
+for (let image = document.getElementsByTagName('img'), j = 0, lj = image.length; j < lj; j++) {
     image[j].setAttribute('width', currentWidth + 'px');
+}
+
+function moveToSlide(target) {
+    //перемещаем к слайду по индикатору
+    target.parentNode.parentNode.getElementsByClassName('content')[0].style.marginLeft = -(target.id * currentWidth) + 'px';
+    //после перемещения снимаем active со всех индикаторов и присваивам на новый
+    for (let x = target.parentNode.getElementsByTagName('LI'), j = 0, lj = x.length; j < lj; j++) {
+        x[j].classList.remove('active');
+    }
+    target.classList.add('active');
+}
+
+function setIndicator() {
+
+    //индикатор становится активным так:
+    //текущий отступ контейнера делим на ширину одного слайда
+    //получаем число которое будет соответсвовать номеру индикатора
+    //например  отступ контейнера 0/щирину то номер индикатора 0
+    //если отступ -675px/ширина(675) = 1(значить слайдер прокрутитили на один слайд)
+    //индикатору с индексом 1 присвоен класс active
+
+
+    let index = Math.abs(parseInt(getComputedStyle(slider).marginLeft));
+
+    for (let unsetInd = slider.parentNode.getElementsByTagName('li'), j = 0, lj = unsetInd.length; j < lj; j++) {
+        unsetInd[j].classList.remove('active');
+    }
+    slider.parentNode.getElementsByTagName('li')[index / currentWidth].classList.add('active');
 }
 
 
 function dragStart() {
+    console.log('start');
+    let startX = event.clientX;
+    let params = {
+        currentMargin: null,
+        maxMargin: currentWidth * slider.getElementsByTagName('img').length - currentWidth,
+        imgLength: slider.getElementsByTagName('img').length,
+    };
+
+    function currentMarginFn() {
+        let x = getComputedStyle(slider);
+        return params.currentMargin = parseInt(x.marginLeft);
+    }
 
     event.preventDefault();
-
-    var currentMargin;
-    var imgLength = slider.getElementsByTagName('img').length;
-    var maxMargin = currentWidth * imgLength - currentWidth;
-    var startX = event.clientX;
 
     document.addEventListener('mousemove', mouseMove);
     document.addEventListener('mouseup', moveEnd);
 
-    function currentMarginFn() {
-        var x = getComputedStyle(slider);
-        return currentMargin = parseInt(x.marginLeft);
-    }
-
     function mouseMove() {
-        var move = startX - event.clientX;
-
-        if (((slider.style.marginLeft = -move + currentMargin) < 0) && ((slider.style.marginLeft = -move + currentMargin) > -maxMargin)) {
-            slider.style.marginLeft = -move + currentMargin + 'px';
+        let move = startX - event.clientX;
+        if (((slider.style.marginLeft = -move + params.currentMargin) < 0) && ((slider.style.marginLeft = -move + params.currentMargin) > -params.maxMargin)) {
+            slider.style.marginLeft = -move + params.currentMargin + 'px';
         }
     }
 
     function moveEnd() {
+        slider.classList.add('transition');// добавим transition после того как сладйр отпустили, раньше нельзя, будет не корректное перетаскивание мышкой
 
-        slider.classList.add('transition');
-
-        var endX = event.clientX;
+        let endX = event.clientX;
 
         document.removeEventListener('mousemove', mouseMove);
 
         if (endX < startX) {
-            if (currentMargin !== -maxMargin) {
-                var left = currentWidth - currentMargin;
+            //выясним напрваление разницей точки в которой был mousedown и mouseup
+            //если точка старта по X например больше точки конца, значить танут влево
+            //выясняем сколько уже протянуто, и сколько осталось
+            //остаток присваиваем в свойство marginLeft
+            if (params.currentMargin !== -params.maxMargin) {
+                let left = currentWidth - params.currentMargin;
                 slider.style.marginLeft = -left + 'px';
             }
             else {
@@ -59,21 +104,20 @@ function dragStart() {
             }
         }
         else if (endX > startX) {
-            if (currentMargin !== 0) {
-                var right = currentWidth + currentMargin;
+            if (params.currentMargin !== 0) {
+                let right = currentWidth + params.currentMargin;
                 slider.style.marginLeft = right + 'px';
             }
             else {
-                slider.style.marginLeft = -maxMargin + 'px';
+                slider.style.marginLeft = -params.maxMargin + 'px';
             }
         }
-
-        currentMarginFn();
+        currentMarginFn(); //обновляем значение margin
+        setTimeout(setIndicator, 250);// вызов присвоения нового индикатора должен быть слегка позже, что бы transition успел закончить
+        document.removeEventListener('mouseup', moveEnd);
     }
-    slider.classList.remove('transition');
-
-    currentMarginFn();
-
+    slider.classList.remove('transition'); //нужно снова удалить для красивого перетасивание указателем
+    currentMarginFn();//обновляем значение margin
 }
 
 
